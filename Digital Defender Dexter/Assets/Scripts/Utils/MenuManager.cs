c00ace74878494b48f8791162a5d7ba39b739b8a
ArchaicEditor <Legojag@icloud.com>
2026-04-20 13:09:49 -0400

    Week 13 second commit
    Added level select screen to main menu and empty scene for levels 2 and 3 to be filled out next week

--- Digital Defender Dexter/Assets/Scripts/Utils/MenuManager.cs
+++ Digital Defender Dexter/Assets/Scripts/Utils/MenuManager.cs
@@ -3,9 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void NewGame()
+
+    public GameObject startMenu;
+    public GameObject levelSelect;
+    public void LevelSelect()
+    {
+        startMenu.SetActive(false);
+        levelSelect.SetActive(true);
+        
+    }
+    public void Level1()
     {
         SceneManager.LoadScene("Level 1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level 3");
     }
 
     public void QuitGame()
